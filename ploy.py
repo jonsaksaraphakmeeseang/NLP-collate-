f7190c3b196b3cd57d14f3b8746a22162ceda5ca
jonsaksaraphakmeeseang <bsim338@gmail.com>
2019-04-29 16:12:35 +0700

    Add files via upload

---
+++ ploy.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 27 14:51:05 2019
+
+@author: maxga
+"""
+
+data = ['and','go','blue','another','bark','cut','disk','baby','dive','excuse','host','image','follow','hero','joke','king','list','meet','notebook','one','pig','quiz','room','set','use','very','tour','venom','walk','xmas','thai','japan','china','friends','sleep','boy','ploy','pam','pim','หลอด','นก','ควาย']
+data.sort() #เรียงข้อมูลที่อยู่ในดาต้า
+print(data) #แสดงข้อมูล
